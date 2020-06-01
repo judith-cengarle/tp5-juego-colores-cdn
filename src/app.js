a6a42558c92374ecc4c7576c386c7801243ba0e6
Judith Cengarle <judithcengarle@gmail.com>
2020-06-01 16:43:19 -0300

    generate colour logic + adding props to Header component

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ Vue.component('Square', {
 
 Vue.component('Container', {
 
-
     template: `
         <div id="container"> 
             <slot/>
@@ -33,12 +32,15 @@ Vue.component('Navigator', {
 
 Vue.component('Header', {
 
+    props: {
+        title : String 
+    },
     template: `
         <div id="header">
 
             <h1>The Great 
             <br>
-            <span id="colorDisplay">RGB</span>
+                <span id="colourDisplay">{{title}}</span>
             <br> 
             Guessing Game</h1>
             
@@ -57,12 +59,54 @@ Vue.component('App', {
                 4,
                 5,
                 6
-            ]
+            ],
+            colourCount : 6,
+            colours : [],
+            pickedColour : null, //temporal value
+            isHard : true
         }
     },
+    mounted : function() {
+        this.colours = this.createNewColours(this.colourCount);
+        this.pickedColour = this.colours[this.pickColour()];
+
+    },
+
+    methods : {
+        pickColour : function() {
+            var quantity;
+            if (this.isHard) {
+                quantity = 6;
+            } else {
+                quantity = 3;
+            }
+            return Math.floor(Math.random() * quantity );
+        },
+        createNewColours : function(numbers) { 
+            var arr = [];
+            for (var i = 0; i < numbers; i++) {
+                arr.push(this.createRandomStringColour());
+            }
+            return arr;
+        },
+        createRandomStringColour: function() {
+            var newColour = "rgb(" + this.randomInt() + ", " + this.randomInt() + ", " +
+            this.randomInt() + ")" ;
+            return newColour;
+        },
+        randomInt : function() {
+            return Math.floor(Math.random() * 256);
+        }
+
+        // restart : function() {
+
+        // }
+
+    },
+
     template: `
         <div>
-            <Header/>
+            <Header :title="pickedColour"/>
             <Navigator/>
             <Container> <Square v-for="square in squares"/> </Container>
             
